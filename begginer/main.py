94f6d06389cb1a3c4f00d73a813d1e5e21bede1a
m1k1e <mikle.fedorenko@gmail.com>
2017-11-05 23:55:18 +0200

    replace pack for place method for button and aligne button center

--- begginer/main.py
+++ begginer/main.py
@@ -3,20 +3,35 @@ Created on 2.11.2017
 
 @author: m1k1e
 '''
-from Tkinter import *
+
+
+from Tkinter import * 
+
 
 def Hello(event):
     print ("Hello, User")
 
+
 root=Tk()
 
-#first comment
+
+root.title('First GUI program')
+
+
+root.geometry("300x200")
+
 
 btn = Button(root,                  
              text="Click me",       
-             width=30,height=5,     
-             bg="white",fg="black") 
-btn.bind("<Button-1>", Hello)       
-btn.pack()                     
+             width=10,
+             height=3,     
+             bg="white",
+             fg="black",              
+             font="12"       
+             ) 
 
-root.mainloop()
+
+btn.bind("<Button-1>", Hello)       
+btn.place(relx=.5, rely=.5, anchor="c", height=30, width=130, bordermode=OUTSIDE)                  
+
+root.mainloop() 
